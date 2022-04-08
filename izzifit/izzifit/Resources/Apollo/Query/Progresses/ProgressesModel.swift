20b202eaa0c4f437044b6f518f529a13c363ec8f
Andrey.S <andrey.s@mail.int>
2022-04-08 12:55:58 +0300

    fix

--- izzifit/izzifit/Resources/Apollo/Query/Progresses/ProgressesModel.swift
+++ izzifit/izzifit/Resources/Apollo/Query/Progresses/ProgressesModel.swift
@@ -12,6 +12,19 @@ struct ProgressesModel: Codable {
 }
 
 struct ProgressesMainModel: Codable {
-    let createdAt: String?
+    let createdAt: String
     let progress: Int?
+    
+    var date: Int? {
+        let oldDateFormatter = DateFormatter()
+        oldDateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        oldDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        guard let date = oldDateFormatter.date(from: createdAt) else {
+            return nil
+        }
+        
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.day], from: date)
+        return components.day
+    }
 }
