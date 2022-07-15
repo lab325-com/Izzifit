093ca4c8fdaf20bdebd61492ef5c351b9955207e
Alex <alex.terode@gmail.com>
2022-07-15 14:42:21 +0300

    fix

--- izzifit/R.generated.swift
+++ izzifit/R.generated.swift
@@ -9289,6 +9289,7 @@ struct _R: Rswift.Validatable {
       }
 
       static func validate() throws {
+        if UIKit.UIImage(named: "energy_arrow_down_ic", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: "[R.swift] Image named 'energy_arrow_down_ic' is used in nib 'EnergyMealsCell', but couldn't be loaded.") }
         if UIKit.UIImage(named: "energy_info_ic", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: "[R.swift] Image named 'energy_info_ic' is used in nib 'EnergyMealsCell', but couldn't be loaded.") }
         if UIKit.UIImage(named: "energy_water_flash_ic", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: "[R.swift] Image named 'energy_water_flash_ic' is used in nib 'EnergyMealsCell', but couldn't be loaded.") }
         if UIKit.UIImage(named: "onboarding_selected_ic", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: "[R.swift] Image named 'onboarding_selected_ic' is used in nib 'EnergyMealsCell', but couldn't be loaded.") }
@@ -9395,6 +9396,7 @@ struct _R: Rswift.Validatable {
       }
 
       static func validate() throws {
+        if UIKit.UIImage(named: "energy_arrow_down_ic", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: "[R.swift] Image named 'energy_arrow_down_ic' is used in nib 'EnergyStepsCell', but couldn't be loaded.") }
         if UIKit.UIImage(named: "energy_info_ic", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: "[R.swift] Image named 'energy_info_ic' is used in nib 'EnergyStepsCell', but couldn't be loaded.") }
         if UIKit.UIImage(named: "energy_water_flash_ic", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: "[R.swift] Image named 'energy_water_flash_ic' is used in nib 'EnergyStepsCell', but couldn't be loaded.") }
         if #available(iOS 11.0, tvOS 11.0, *) {
