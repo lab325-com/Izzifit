529d063ee009c5589e23a1c3aad8f04565716a1e
olehDovhan <odovhan@3knbh.com>
2022-08-11 10:34:00 +0300

    tooltips layout fixed

--- izzifit/izzifit/Controller/Game/MainGameOnboardingView/MainGameOnboardingView.swift
+++ izzifit/izzifit/Controller/Game/MainGameOnboardingView/MainGameOnboardingView.swift
@@ -57,7 +57,7 @@ class MainGameOnboardingView: UIView {
         onboardImgVw.contentMode = .scaleAspectFill
         layer.backgroundColor = UIColor.clear.cgColor
         change(by: state)
-    //    mainBtn.backgroundColor = .yellow.withAlphaComponent(0.75)
+   //     mainBtn.backgroundColor = .yellow.withAlphaComponent(0.89)
         mainBtn.addTarget(self,
                           action: #selector(btnTapped),
                           for: .touchUpInside)
@@ -230,27 +230,59 @@ class MainGameOnboardingView: UIView {
                               centerH: 0)
         case .level4:
             
-            if let levelVC = self.gameTabBar?.children[1] as? LevelController {
-                levelVC.levelView.onboardLevelFour()
-                }
+            if let levelVC = self.gameTabBar?.children[1] as? LevelController { levelVC.levelView.onboardLevelFour() }
 
-            onboardImgVw.image = image(img: .onUpgradeBuild)
-              
-            ui.genericlLayout(object: onboardImgVw,
-                              parentView: self,
-                              width: 228,
-                              height: 97,
-                              topC: 315,
-                              centerH: 51)
+            let window = UIApplication.shared.windows[0]
+            let safeFrame = window.safeAreaLayoutGuide.layoutFrame
+            var topSafeAreaHeight = safeFrame.minY
+            var bottomSafeAreaHeight = window.frame.maxY - safeFrame.maxY
             
-            onboardImgVw.shake(0.5, x: w / 2 + 51, y: 315 + 48.5)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.33) {
             
-            ui.genericlLayout(object: mainBtn,
-                             parentView: self,
-                             width: 110,
-                             height: 110,
-                             topC: 390,
-                             trailingC: 58)
+                switch self.h {
+                case 926.0:
+                    self.onboardImgVw.image = self.image(img: .onBuildVillage)
+                    self.ui.genericlLayout(object: self.onboardImgVw,
+                                           parentView: self,
+                                           width: 228,
+                                           height: 97,
+                                           topC: 315,
+                                           centerH: 53.5)
+             self.onboardImgVw.shake(0.5,
+                                     x: self.w / 2 + 53.5,
+                                     y: 315 + 48.5)
+
+                default:
+                    self.onboardImgVw.image = self.image(img: .onBuildVillage)
+                    self.ui.genericlLayout(object: self.onboardImgVw,
+                                           parentView: self,
+                                           width: 228,
+                                           height: 97,
+                                           topC: 315 - topSafeAreaHeight,
+                                           centerH: 53.5)
+                    
+             self.onboardImgVw.shake(0.5,
+                                     x: self.w/2 + 53.5,
+                                     y: (315 - topSafeAreaHeight) + 48.5)
+                }
+            }
+           
+            switch h {
+            case 926.0:
+                ui.genericlLayout(object: mainBtn,
+                                 parentView: self,
+                                 width:  170,
+                                 height: 138,
+                                 topC:   375,
+                                 centerH: 81.5)
+            default:
+                ui.genericlLayout(object: mainBtn,
+                                 parentView: self,
+                                 width:  170,
+                                 height: 138,
+                                 topC:   375 - topSafeAreaHeight,
+                                 centerH: 81.5)
+            }
             
         case .buildPopUp5:  ui.genericlLayout(object: mainBtn,
                                                 parentView: self,
